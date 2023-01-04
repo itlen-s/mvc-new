ab1859cfd6b6d7e3098e7775989644f9acc6da41
itlen-s <nabiullin-ildus@mail.ru>
2023-01-04 20:26:00 +0500

    stage-8

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -8,13 +8,17 @@ abstract class Controller {
 
     public $route;
     public $view;
+    public $acl;
 
     public function __construct($route) {
-        $this->route = $route;
         // var_dump($route);
         // echo '<p>hi<p>';
         // debug($this->route);
         $this->route = $route;
+        // $_SESSION['authorize']['id'] =1;
+        if (!$this->checkAcl()) {
+            View::errorCode(403);
+        };
         $this->view = new View($route);
         $this->model = $this->loadModel($route['controller']);
         // debug($this->model);
@@ -28,6 +32,28 @@ abstract class Controller {
             return new $path;
         }
         // debug($path);
+    }
+
+    public function checkAcl() {
+        // $path = $this->route['controller'].'.php';
+        
+        // if (file_exists($path)) {
+            $this->acl = require 'application/acl/'.$this->route['controller'].'.php';
+            if ($this->isAcl('all')) {
+                return true;
+            } elseif (isset($_SESSION['authorize']['id']) and $this->isAcl('authorize')) {
+                return true;
+            } elseif (!isset($_SESSION['authorize']['id']) and $this->isAcl('guest')) {
+                return true;
+            } elseif (isset($_SESSION['admin']) and $this->isAcl('guest')) {
+                return true;
+            } 
+            return false;
+        // debug($acl);
+    }
+
+    public function isAcl($key) {
+        return in_array($this->route['action'], $this->acl[$key]);
     }
 
 }
